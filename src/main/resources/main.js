4e948a8eeac2dc3d82de5a102eac3006018eb72c
Dudu <eduardo@bruno.etc.br>
2018-04-04 17:21:15 -0300

    new main spike

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,3 +1,42 @@
+const __sharedData = {
+    cols: 2,
+    refreshRate : 120,
+    showMonitorTop : true
+}
+
+var monitorCfg = new Vue({
+  el: '#monitorCfg',
+  created: function() {
+    var loadCol = localStorage.getItem('cols')
+    this.updateCols(parseInt( loadCol || 2 ));
+  },
+  data: {
+    sharedData: __sharedData
+  },
+  methods: {
+     colStyle: function() {
+        return 'col-md-' + (12 / this.sharedData.cols);
+    },
+    updateCols: function(n) {
+         localStorage.setItem('cols', n);
+         this.sharedData.cols = n;
+    },
+    updateShowMonitorTop: function(n) {
+         //localStorage.setItem('cols', n);
+         //this.sharedData.cols = n;
+         console.log(n);
+    }
+  },
+  watch: {
+    'sharedData.cols': function(val, oldVal) {
+        localStorage.setItem('cols', val);
+    },
+    'sharedData.refreshRate': function(val, oldVal) {
+        localStorage.setItem('refreshRate', val);
+    }
+  }
+});
+
 var app1 = new Vue({
   el: '#serverComponent',
   created: function() {
@@ -5,7 +44,7 @@ var app1 = new Vue({
     var self = this;
 
     var reload = function() {
-        axios.get("http://localhost:9999/api/v2").then(response => {
+        axios.get("http://10.42.12.136:7777/api/v2").then(response => {
           self.servers = response.data;
         });
     };
@@ -15,8 +54,15 @@ var app1 = new Vue({
   data:
     function () {
         return  {
-            servers: []
+            servers: [],
+            cols: 2,
+            sharedData: __sharedData
         }
   },
-  methods: {}
+  methods: {
+     colStyle: function() {
+        //return 'col-md-' + (12 / this.cols);
+        return 'col-md-' + (12 / this.sharedData.cols);
+    }
+  }
 });
